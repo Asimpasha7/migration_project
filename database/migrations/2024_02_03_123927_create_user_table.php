a2a323eed67e62aa1496ed1c7cacbda347f9cda1
IADLP-39\Asim <asim.pasha@ibridgellc.com>
2024-02-03 18:24:22 +0530

    first commit

---
+++ database/migrations/2024_02_03_123927_create_user_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUserTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('user', function (Blueprint $table) {
+            $table->id();
+            $table->string('user_name');
+            $table->string('password');
+            $table->foreignId('role_id')->constrained();
+            $table->foreignId('vendor_d')->nullable()->constrained;
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('user');
+    }
+}
